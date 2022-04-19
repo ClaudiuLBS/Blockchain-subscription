7e25c7d776316c1269c6b1e81348ab603d762727
Claudiu <clau_123@yahoo.com>
2022-04-19 10:23:32 +0300

    first commit

---
+++ blockchain/contracts/Subscription.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+contract Subscription {
+  
+  mapping(address => bool) subscribers;
+  uint subscriptionPrice;
+  uint rewardValue;
+  constructor( uint _subscriptionPrice, uint _rewardValue) {
+    subscriptionPrice = _subscriptionPrice;
+    rewardValue = _rewardValue;
+  }
+
+  event SomeoneSubscribed(address subscriber);
+
+  function subscribe() public payable {
+    require(uint(msg.value) == subscriptionPrice, "Not correct price");
+    require(subscribers[msg.sender] == false, "You can have only one subscription at a time");
+    subscribers[msg.sender] = true;
+    emit SomeoneSubscribed(msg.sender);
+  }
+
+  function subscribeToCurrentChain(address _subscriber) public {
+    require(subscribers[_subscriber] == false, "You can have only one subscription at a time");
+    token.safeTransferFrom(msg.sender, address(this), subscriptionPrice);
+
+    subscribers[_subscriber] = true;
+  } 
+
+}
