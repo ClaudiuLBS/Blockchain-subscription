6bd87cc827b4e350161f828c3865704b679a0793
Claudiu <clau_123@yahoo.com>
2022-04-27 13:51:08 +0300

    a bit of code refactoring

--- blockchain/test/tests.js
+++ blockchain/test/tests.js
@@ -1,32 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-async function wait(days) {
-  await network.provider.send("evm_increaseTime", [86400 * days]);
-  await network.provider.send("evm_mine");
-}
-
 describe("Subscription", function () {
   it("Should work", async function () {
     const Subscription = await ethers.getContractFactory("Subscription");
-    const subscription = await Subscription.deploy(
-      ethers.utils.parseEther("0.1"),
-      ethers.utils.parseEther("0.001")
-    );
+    const subscription = await Subscription.deploy(ethers.utils.parseEther("0.1"), ethers.utils.parseEther("0.001"));
     await subscription.deployed();
-
-    // const rinkebyProvider = ethers.getDefaultProvider(
-    //   "https://rinkeby.infura.io/v3/9b002978561d42a0a220ddca7f38893b"
-    // );
-    // const bscProvider = ethers.getDefaultProvider(
-    //   "https://data-seed-prebsc-1-s1.binance.org:8545"
-    // );
-    // const mumbaiProvider = ethers.getDefaultProvider(
-    //   "https://polygon-mumbai.g.alchemy.com/v2/OCfh91vbINd8BDWsl7BWtKXtds2hqmTV"
-    // );
-
-    // rinkebyProvider;
-    // console.log(ethers.utils.parseUnits("0.1", "bnb"));
-    await subscription.subscribe({ value: ethers.utils.parseEther("0.1") });
+    console.log(process.argv[4]);
   });
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import { ethers } from "ethers";
 import { Alert, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 
-import "./App.css";
+import "./styles/App.css";
 import switchNetwork from "./utils/switchNetwork";
 import getContractByProvider from "./utils/getContractByProvider";
 import LoadingScreen from "./components/LoadingScreen";
 
 const day = 10;
-
 const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
@@ -24,9 +23,11 @@ const App = () => {
         await window.ethereum.request({
           method: "eth_requestAccounts",
         });
+
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const { chainId } = await provider.getNetwork();
         setCurrentChain(parseInt(chainId));
+
         setErrorMessage("");
       } catch (error) {
         setErrorMessage("Connect to your account");
@@ -47,21 +48,18 @@ const App = () => {
     const contract = await getContractByProvider(provider);
 
     let price = "0.1";
-    if (currentChain == 97) price = "0.06";
-    else if (currentChain == 80001) price = "0.2";
+    if (currentChain === 97) price = "0.06";
+    else if (currentChain === 80001) price = "0.2";
 
-    try {
-      const data = await contract.subscribe({
-        gasLimit: 300000,
-        value: ethers.utils.parseEther(price), //aici va trebui o functie care alege valoarea abonamentului in functie de retea
-      });
-      setLoading(true);
-      provider.once(data.hash, (transaction) => {
-        setLoading(false);
-      });
-    } catch (err) {
-      console.log("Error: ", err);
-    }
+    const data = await contract.subscribe({
+      gasLimit: 300000,
+      value: ethers.utils.parseEther(price),
+    });
+
+    setLoading(true);
+    provider.once(data.hash, (transaction) => {
+      setLoading(false);
+    });
   };
 
   const claim = async () => {
@@ -69,75 +67,62 @@ const App = () => {
     const contract = await getContractByProvider(provider);
     const myAddress = await provider.getSigner().getAddress();
 
-    let bigNumber;
-    bigNumber = await contract.getNextClaimDate(myAddress);
-    let nextClaimDate = parseInt(bigNumber._hex);
+    let nextClaimDate = await contract.getNextClaimDate(myAddress);
+    nextClaimDate = parseInt(nextClaimDate._hex);
 
-    bigNumber = await contract.getRegistrationDate(myAddress);
-    const registrationDate = parseInt(bigNumber._hex);
-    const lastClaimDate = registrationDate + 300;
+    let registrationDate = await contract.getRegistrationDate(myAddress);
+    registrationDate = parseInt(registrationDate._hex);
+    const lastClaimDate = registrationDate + 30 * day;
 
     let totalRewards = 0;
-    while (
-      nextClaimDate < Math.floor(Date.now() / 1000) &&
-      nextClaimDate <= lastClaimDate
-    ) {
+    while (nextClaimDate < Math.floor(Date.now() / 1000) && nextClaimDate <= lastClaimDate) {
       totalRewards++;
       nextClaimDate += day;
     }
 
-    try {
-      console.log(totalRewards);
-      if (totalRewards > 0)
-        try {
-          await contract.claim(myAddress, totalRewards, nextClaimDate, {});
-        } catch (err) {
-          await contract.claim(myAddress, totalRewards, nextClaimDate, {
-            gasLimit: 400000,
-          });
-        }
-    } catch (err) {
-      console.log("Error: ", err);
-    }
+    if (totalRewards > 0)
+      try {
+        const data = await contract.claim(myAddress, totalRewards, nextClaimDate, {});
+        setLoading(true);
+        provider.once(data.hash, (transaction) => {
+          setLoading(false);
+        });
+      } catch (err) {
+        const data = await contract.claim(myAddress, totalRewards, nextClaimDate, {
+          gasLimit: 400000,
+        });
+        setLoading(true);
+        provider.once(data.hash, (transaction) => {
+          setLoading(false);
+        });
+      }
+    else console.log("0 rewards");
   };
 
-  if (errorMessage.length > 0)
-    return <Alert severity="error">{errorMessage}</Alert>;
+  const pickButtonFunction = (chain, network) => {
+    if (currentChain === chain) return () => claim(network);
+    else return () => switchNetwork(network);
+  };
+
+  if (errorMessage.length > 0) return <Alert severity="error">{errorMessage}</Alert>;
 
   return (
     <div className="App">
       <div className="container">
-        <Button
-          className="subscribeButton"
-          color="success"
-          variant="contained"
-          onClick={() => subscribe()}
-        >
+        <Button className="subscribeButton" color="success" variant="contained" onClick={() => subscribe()}>
           Subscribe
         </Button>
-        <Button
-          variant="contained"
-          onClick={() =>
-            currentChain == 4 ? claim("rinkeby") : switchNetwork("rinkeby")
-          }
-        >
-          {currentChain == 4 ? "Claim ETH" : "Switch to rinkeby"}
+
+        <Button variant="contained" onClick={pickButtonFunction(4, "rinkeby")}>
+          {currentChain === 4 ? "Claim ETH" : "Switch to rinkeby"}
         </Button>
-        <Button
-          variant="contained"
-          onClick={() =>
-            currentChain == 97 ? claim("bsc") : switchNetwork("bsc")
-          }
-        >
-          {currentChain == 97 ? "Claim BNB" : "Switch to bsc testnet"}
+
+        <Button variant="contained" onClick={pickButtonFunction(97, "bsc")}>
+          {currentChain === 97 ? "Claim BNB" : "Switch to bsc testnet"}
         </Button>
-        <Button
-          variant="contained"
-          onClick={() =>
-            currentChain == 80001 ? claim("mumbai") : switchNetwork("mumbai")
-          }
-        >
-          {currentChain == 80001 ? "Claim MATIC" : "Switch to mumbai"}
+
+        <Button variant="contained" onClick={pickButtonFunction(80001, "mumbai")}>
+          {currentChain === 80001 ? "Claim MATIC" : "Switch to mumbai"}
         </Button>
       </div>
       <LoadingScreen open={loading} />
